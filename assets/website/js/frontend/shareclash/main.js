a7b1f247da080f67882c6ef2bb74e0208da9f886
hengdashuang <hengdashuang@outlook.com>
2025-02-08 07:49:52 +0800

    bu kuo zhan biao zhi xing dian zi chan pin zu he de li cheng bei wo men hen gao xing zhao dao le yi

--- assets/website/js/frontend/shareclash/main.js
+++ assets/website/js/frontend/shareclash/main.js
@@ -185,13 +185,7 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
+  
   /**
    * Portfolio details slider
    */
